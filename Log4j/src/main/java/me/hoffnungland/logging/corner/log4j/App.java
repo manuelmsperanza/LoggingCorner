179f55c04ddd2a46f95b21d1d37eea6ef7275edc
manuelmsperanza <manuel.m.speranza@gmail.com>
2018-11-22 11:55:20 +0100

    first version

--- Log4j/src/main/java/me/hoffnungland/logging/corner/log4j/App.java
+++ Log4j/src/main/java/me/hoffnungland/logging/corner/log4j/App.java
@@ -1,13 +1,19 @@
 package me.hoffnungland.logging.corner.log4j;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static final Logger logger = LogManager.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	logger.traceEntry();
+    	logger.info( "Hello World!" );
+    	logger.traceExit();
     }
 }
