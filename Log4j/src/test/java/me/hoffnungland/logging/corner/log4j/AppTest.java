ba452d8f323ce154fa1391f28c57b11817ac525a
manuelmsperanza <manuel.m.speranza@gmail.com>
2018-11-22 11:37:35 +0100

    first commit

---
+++ Log4j/src/test/java/me/hoffnungland/logging/corner/log4j/AppTest.java
@@ -0,0 +1,20 @@
+package me.hoffnungland.logging.corner.log4j;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
