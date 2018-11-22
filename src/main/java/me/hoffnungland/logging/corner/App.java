c9eaf62aadca6fd059d007b4136527effb9a88cd
manuelmsperanza <manuel.m.speranza@gmail.com>
2018-11-22 11:37:35 +0100

    first commit

---
+++ src/main/java/me/hoffnungland/logging/corner/App.java
@@ -0,0 +1,13 @@
+package me.hoffnungland.logging.corner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
